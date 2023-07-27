1a7d20d7edaa538e891de27ab0dac3bf9f30d24b
mrg <rgmengedoth@gmail.com>
2023-07-27 17:21:45 +0200

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,60 @@
 
 import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
 
-function App() {
+    type Character = {
+        character: {
+            id: string;
+            name: string;
+            image: string;
+            origin: {
+                name: string;
+            };
+        }
+    }
 
-}
+    function AllCharacters() {
+        const [characters, setCharacters] = useState<undefined|Character[]>(undefined);
+        useEffect(() =>{
+            axios({
+                method: 'get',
+                url: 'https://rickandmortyapi.com/api/character',
+            })
+                .then(function (response) {
+                    console.log("GET / READ")
+                    console.log("Response status: ",response.status)
+                    console.log("Response data: ",response.data)
+                    setCharacters(response.data.results)
+                });
+        },[]);
+        return (characters === undefined ? <div>Loading Characters</div> : (
+            <ul className={"gallery"}>
+                {characters.map(char =>
+                        <li key={char.id} className={"character-card"}>
+                            <h2>{char.name}</h2>
+                            <img src={char.image} alt={"Avatar"}/>
+                            <h3>{char.origin.name}</h3>
+                        </li>
+                    )}
+            </ul>
+
+        ));
+    }
+
+    function Ueberschrift() {
+        return (<div>
+            <h1>Rick and Morty + AXIOS APP</h1>
+        </div>);
+    }
+
+
+    function App() {
+        return<div>
+            <Ueberschrift></Ueberschrift>
+            <AllCharacters></AllCharacters>
+        </div>
+
+    }
 
 export default App
